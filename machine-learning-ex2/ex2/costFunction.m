adb1fd67e42ae537851db0a1dcfd728f065771cd
dlosowyj <djl256@cornell.edu>
2017-05-18 14:59:41 -0500

    Added functionality to machine-learning-ex2/ex2/costFunction.m. It now correctly computes the cost function.

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -20,7 +20,7 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
-J = 1/m*sum(-log(sigmoid(X*theta))*y'-log(1-sigmoid(X*theta))*(1-y'));
+J = 1/m*sum(-y'*log(sigmoid(X*theta))-(1-y')*log(1-sigmoid(X*theta)));
 
 grad = 1/m*(sigmoid(X*theta)-y)'*X;
 
