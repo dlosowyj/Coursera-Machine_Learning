5d2d0e89ee93e8a3f3c077092ca4c05f1149ec80
dlosowyj <djl256@cornell.edu>
2017-06-10 16:20:57 -0500

    All files in ex7 are now fully implemented.

--- machine-learning-ex7/ex7/recoverData.m
+++ machine-learning-ex7/ex7/recoverData.m
@@ -21,7 +21,8 @@ X_rec = zeros(size(Z, 1), size(U, 1));
 %               Notice that U(j, 1:K) is a row vector.
 %               
 
-
+v = Z(1:end, :)';
+X_rec = v'*U(:, 1:K)';
 
 % =============================================================
 
